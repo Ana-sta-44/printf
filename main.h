f539e0042a0cc60fd60aaab43cbd19b388e93f30
Ana-sta-44 <ugwuokechisom123@gmail.com>
2023-06-27 04:50:09 -0700

    my main.h

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 int print(char c);
-int _printf(const char *format,...);
+int _printf(const char *format, ...);
 int print_string(char *s);
 void print_digit(int num);
 int print_num(int num);
