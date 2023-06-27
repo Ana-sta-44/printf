f31e84143b052de34c6d1d4dd56e733e4bda4683
Harrison <harrisonchialuka@gmail.com>
2023-06-27 10:36:10 +0000

    printf project

--- main.h
+++ main.h
@@ -5,9 +5,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-
 int print(char c);
-int _printf(const char *format, ...);
+int _printf(const char *format,...);
 int print_string(char *s);
+void print_digit(int num);
+int print_num(int num);
 
 #endif
